995e222f96e68b241ff7861bb494c4f6350ddd3d
Luis Rivera <lrivera@optecllc.com>
2023-11-24 13:00:49 -0500

    python3 13_map.py

--- 13_map.py
+++ 13_map.py
@@ -17,3 +17,41 @@ doubled = map(lambda x: x * 2, numbers)
 
 print(list(doubled))
 
+# Path: 14_filter.py
+
+numbers = [1, 2, 3, 4, 5]
+
+def is_even(x):
+    return x % 2 == 0
+
+even = filter(is_even, numbers)
+
+print(list(even))
+
+items = [
+    {
+        'product': 'Rice',
+        'price': 10,
+        'quantity': 5
+    },
+    {
+        'product': 'Wheat',
+        'price': 20,
+        'quantity': 10
+    },
+    {
+        'product': 'Sugar',
+        'price': 30,
+        'quantity': 15
+    }
+]
+
+prices = map( lambda x : x['price'], items )
+
+new_items = list(map(lambda item: {**item, "fee_with_fee": item['price'] * 0.16}, items))
+
+print( list(prices))
+
+print( new_items )
+
+print(items)
